6ba4c61949263f8af5484365cb84fe77db8a11bc
padong2023 <128555332+padong2023@users.noreply.github.com>
2023-05-15 02:17:34 +0000

    switch문 추가

--- .ccls-cache/@home@runner@3-9/condition_multiple.c
+++ .ccls-cache/@home@runner@3-9/condition_multiple.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-main(){
+// #include <stdio.h>
+// main(){
 
-  int a;
-  printf("1:사과, 2:키위, 3:복숭아, 4:수박\n");
-  printf("자신이 좋아하는 과일을 번호로 입력해주세요.");
-  scanf("%d", &a);
+//   int a;
+//   printf("1:사과, 2:키위, 3:복숭아, 4:수박\n");
+//   printf("자신이 좋아하는 과일을 번호로 입력해주세요.");
+//   scanf("%d", &a);
   
-	if(a==1)
-		printf("사과를 좋아하는군요!\n"); 
-	else if(a==2)
-		printf("키위를 좋아하는군요!\n");
-	else if(a==3)
-		printf("복숭아를 좋아하는군요!\n");
-	else if(a==4)
-		printf("수박를 좋아하는군요!\n");
-	else
-		printf("당신은 아마도 과일을 좋아하지 않는 것 같군요\n");
-	printf("좋아하는 과일 조사 끝"); 
-}
+// 	if(a==1)
+// 		printf("사과를 좋아하는군요!\n"); 
+// 	else if(a==2)
+// 		printf("키위를 좋아하는군요!\n");
+// 	else if(a==3)
+// 		printf("복숭아를 좋아하는군요!\n");
+// 	else if(a==4)
+// 		printf("수박를 좋아하는군요!\n");
+// 	else
+// 		printf("당신은 아마도 과일을 좋아하지 않는 것 같군요\n");
+// 	printf("좋아하는 과일 조사 끝"); 
+// }
